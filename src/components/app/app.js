a5f51ae10dba64de5e7e31c3dba2cc4557ed4861
Aleksandr <it.kucherov@gmail.com>
2022-02-20 22:03:30 +0300

    practic props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployersList from '../employers-list/employers-list';
 import EmployeesAddForm from '../employers-add-form/employers-add-form';
 
 function App() {
+	const data = [
+		{ name: 'John C.', salary: 800, increase: false },
+		{ name: 'Alex M.', salary: 3000, increase: true },
+		{ name: 'Carl W.', salary: 5000, increase: false },
+		{ name: 'Clark S.', salary: 8000, increase: true },
+	];
+
 	return (
 		<div className="app">
 			<AppInfo />
@@ -14,7 +21,7 @@ function App() {
 				<SearchPanel />
 				<AppFilter />
 			</div>
-			<EmployersList />
+			<EmployersList data={data} />
 			<EmployeesAddForm />
 		</div>
 	);
