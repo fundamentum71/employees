d14eabbc60c0a3dcf0550f1a532189e2580d475b
Aleksandr <it.kucherov@gmail.com>
2022-02-25 20:23:13 +0300

    realis search and filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -29,19 +29,14 @@ class App extends Component {
 				},
 				{
 					name: 'Carl W.',
-					salary: 6000,
+					salary: 5000,
 					increase: true,
 					rise: false,
 					id: 3,
 				},
-				{
-					name: 'Clark S.',
-					salary: 8000,
-					increase: false,
-					rise: false,
-					id: 4,
-				},
 			],
+			term: '',
+			filter: 'all',
 		};
 		this.maxId = 5;
 	}
@@ -53,6 +48,7 @@ class App extends Component {
 			};
 		});
 	};
+
 	addItem = (name, salary) => {
 		const newItem = {
 			name,
@@ -83,19 +79,50 @@ class App extends Component {
 		}));
 	};
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+		return items.filter((item) => {
+			return item.name.indexOf(term) > -1;
+		});
+	};
+
+	onUpdateSearch = (term) => {
+		this.setState({ term });
+	};
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter((item) => item.rise);
+			case 'moreThen1000':
+				return items.filter((item) => item.salary > 1000);
+			default:
+				return items;
+		}
+	};
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter });
+	};
+
 	render() {
+		const { data, term, filter } = this.state;
+
 		const employees = this.state.data.length;
 		const increased = this.state.data.filter((item) => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 		return (
 			<div className="app">
 				<AppInfo employees={employees} increased={increased} />
 
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel onUpdateSearch={this.onUpdateSearch} />
+					<AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
 				</div>
 				<EmployersList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}
 				/>
